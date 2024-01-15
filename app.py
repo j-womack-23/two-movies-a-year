a74df3a0bfdeebce3351fe214e509ac7c3f2b102
j-womack-23 <156448997+j-womack-23@users.noreply.github.com>
2024-01-15 13:32:34 -0600

    Update app.py

--- app.py
+++ app.py
@@ -63,8 +63,7 @@ def main():
     
     display_logo()
 
-    st.title("Two Movies A Year")
-    st.subtitle("Random Movie Selector")
+    st.title("Two Movies A Year: Random Movie Selector")
 
     # Option for user to choose data source
     source_option = st.selectbox("Select data source", ["Upload CSV", "Google Sheets URL"])
